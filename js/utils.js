e5d2c849241ff04be17b727bb9b8e0b26d0c729c
Hakchi06 <miguel_angeleduarte@hotmail.com>
2025-03-04 15:49:56 -0500

    correccion de funcion importada de simplex-noise

--- js/utils.js
+++ js/utils.js
@@ -1,11 +1,11 @@
-import { noise2D } from 'simplex-noise';
+import { createNoise2D } from 'simplex-noise';
 
 function generateHeightMap(width, height, scale = 0.1) {
     let data = [];
     
     for (let y = 0; y < height; y++) {
         for (let x = 0; x < width; x++) {
-            let value = noise2D(x * scale, y * scale);
+            let value = createNoise2D(x * scale, y * scale);
             data.push(value);  // Guardamos el valor de la altura
         }
     }
