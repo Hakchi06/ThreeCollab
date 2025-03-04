107c2c908fed6818320c723d07d38accc043acd7
Hakchi06 <miguel_angeleduarte@hotmail.com>
2025-03-04 15:32:03 -0500

    correccion de uso de SimplexNoise

--- js/utils.js
+++ js/utils.js
@@ -1,6 +1,6 @@
 import * as SimplexNoise from 'https://cdn.jsdelivr.net/npm/simplex-noise@2.4.0/simplex-noise.min.js';
 
-const simplex = new SimplexNoise();
+const simplex = SimplexNoise();
 
 function generateHeightMap(width, height, scale = 0.1) {
     let data = [];
