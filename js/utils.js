72cdef13c8811cae1b5e8836fa5c5e74a5f7ab32
Hakchi06 <miguel_angeleduarte@hotmail.com>
2025-03-04 15:42:13 -0500

    correccion en el uso de simplex-noise

--- js/utils.js
+++ js/utils.js
@@ -1,13 +1,11 @@
-import * as SimplexNoise from 'https://cdn.jsdelivr.net/npm/simplex-noise@2.4.0/simplex-noise.min.js';
-
-const simplex = SimplexNoise();
+import { noise2D } from 'simplex-noise';
 
 function generateHeightMap(width, height, scale = 0.1) {
     let data = [];
     
     for (let y = 0; y < height; y++) {
         for (let x = 0; x < width; x++) {
-            let value = simplex.noise2D(x * scale, y * scale);
+            let value = noise2D(x * scale, y * scale);
             data.push(value);  // Guardamos el valor de la altura
         }
     }
